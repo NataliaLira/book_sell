6a9471741abed826f68fac8a16d90eef176987e5
Sérgio Moura <smouracalmon@gmail.com>
2020-07-02 15:44:31 -0300

    Alterações no css do template

--- resources/views/templates/institucional.blade.php
+++ resources/views/templates/institucional.blade.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset("/css/main.css") }}">
-    <link rel="stylesheet"  href="/css/form.css">
-    <link rel="stylesheet" href="/css/fav.css">
-    <script src="https://use.fontawesome.com/271587175f.js"></script>
+    <link rel="stylesheet" href="/css/main.css">
+    {{--  <link rel="stylesheet" href="/css/form.css">  --}}
+    {{--  <link rel="stylesheet" href="/css/fav.css">  --}}
 </head>
 
 <body>
+    
     <header>
-        <img src="img/book_logo.svg" alt="">
         <nav>
+            <img src="img/book_logo.svg" alt="BookSell">
             <ul>
                 <li><a href="/favorito">Favoritos</a></li>
                 <li><a href="/login">Login</a></li>
@@ -23,11 +23,12 @@
             </ul>
         </nav>
     </header>
+
     @yield('conteudo')
     <footer>
         <p>copyright© - 2020 FavLivros. All rights reserved</p>
     </footer>
-
+    <script src="https://use.fontawesome.com/271587175f.js"></script>
 </body>
 
 </html>
