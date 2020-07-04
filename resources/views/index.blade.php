95b8bfd68bf915998f0385bb2093581c686bc09d
Sérgio Moura <smouracalmon@gmail.com>
2020-07-03 21:19:50 -0300

    novas alterações css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,75 +1,125 @@
 @extends('templates.institucional')
 
-@section('titulo')
-Home
-@endsection
+@section('titulo','Home')
 
 @section('conteudo')
     <main id="home-content">
-        <div>
-            <h3>Todos Os Livros</h3>
-        </div>
+        
+        <h3>Todos os livros</h3>
+        
         <section>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
+            <article>
+                <a href="/livros/1">
+                    <img src="img/quemmexeu.png" alt="Quem Mexeu no Meu Queijo">
+                </a>
                 <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
-                <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
-            <div class="card-livros">
-                <div class="card-img" style="background-image: url('img/quemmexeu.png');"></div>
-                <h3>Quem mexeu no meu queijo?</h3>
-                <button>Favoritar</button>
-                <button>Ver Mais</button>
-            </div>
+                <form>
+                    <button class="btn">
+                        <i class="material-icons">favorite</i>
+                        Favoritar
+                    </button>
+                    <a href="/livros/1" class="btn">
+                        <i class="material-icons">add</i>Ver Mais</a>
+                </form>
+            </article>
         </section>
     </main>
 @endsection
--- resources/views/templates/institucional.blade.php
+++ resources/views/templates/institucional.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css">
     {{--  <link rel="stylesheet" href="/css/form.css">  --}}
-    {{--  <link rel="stylesheet" href="/css/fav.css">  --}}
+    <link rel="stylesheet" href="/css/fav.css">
 </head>
 
 <body>
-    
     <header>
         <nav>
             <img src="img/book_logo.svg" alt="BookSell">
@@ -28,7 +28,6 @@
     <footer>
         <p>copyright© - 2020 FavLivros. All rights reserved</p>
     </footer>
-    <script src="https://use.fontawesome.com/271587175f.js"></script>
 </body>
 
 </html>
